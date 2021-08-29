e77534b6bac7b732e93129c537f323a2f0ed4a05
mouazourassen18 <54610297+mouazourassen18@users.noreply.github.com>
2021-08-29 07:48:20 -0400

    final_end

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,31 +3,30 @@ import Form from "./Form";
 import Login from "./Login";
 
 var userIsRegistered = false;
-
+function Log() {
+  return (
+    <div>
+      <Form type="text" placeholder="username" />
+      <Form type="password" placeholder="password" />
+      {/* <Form type="password" placeholder="confirm" /> */}
+      <Login register="login" />
+    </div>
+  );
+}
+function Reg() {
+  return (
+    <div>
+      <Form type="text" placeholder="username" />
+      <Form type="password" placeholder="password" />
+      <Form type="password" placeholder="confirm Password" />
+      <Login register="Register" />
+    </div>
+  );
+}
 function App() {
   return (
     <div className="container">
-      <form className="form">
-        {/* <Form type="text" placeholder="username" />
-        <Form type="password" placeholder="password" />
-        <Form type="password" placeholder="confirm" />
-        <Login register="login" /> */}
-        {userIsRegistered ? (
-          <div>
-            <Form type="text" placeholder="username" />
-            <Form type="password" placeholder="password" />
-            {/* <Form type="password" placeholder="confirm" /> */}
-            <Login register="login" />
-          </div>
-        ) : (
-          <div>
-            <Form type="text" placeholder="username" />
-            <Form type="password" placeholder="password" />
-            <Form type="password" placeholder="confirm Password" />
-            <Login register="Register" />{" "}
-          </div>
-        )}
-      </form>
+      <form className="form">{userIsRegistered ? <Log /> : <Reg />}</form>
     </div>
   );
 }
